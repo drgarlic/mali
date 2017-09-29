e8e4ebd54779c41f5de3b11f9957e9fe74004e30
gawkish <32246972+gawkish@users.noreply.github.com>
2017-09-29 23:02:42 +0200

    Update fifo.sh

--- fifo.sh
+++ fifo.sh
@@ -4,7 +4,8 @@ clear
 echo -e "\n    Welcome.
   This script will guide you during this very painful installation of Arch Linux.
   Put your belt on, take a deep breath and please try not to panic."
-read -p -e "\n  Press enter to continue"
+read -p -e "
+  Press enter to continue"
 
 echo -e "\n\n    Chapter I - Preparations\n"
 
