e5792f96ce58c644a6205f0dd7a5794c6d2b2ff5
gawkish <32246972+gawkish@users.noreply.github.com>
2017-10-01 13:41:39 +0200

    Update lilo.sh

--- lilo.sh
+++ lilo.sh
@@ -10,6 +10,8 @@ echo "  Installing the window manager..."
 pacaur -S --noconfirm --noedit --silent openbox-patched &> /dev/null
 echo "  Installing the terminal..."
 pacaur -S --noconfirm --noedit --silent termite tmux feh htop openssh rsync scrot &> /dev/null
+echo "  Installing some fonts..."
+pacaur -S --noconfirm --noedit --silent phallus-fonts-git bdf-creep gohufont
 echo "  Installing the text editor..."
 pacaur -S --noconfirm --noedit --silent neovim micro
 echo "  Installing Firefox..."
@@ -34,18 +36,16 @@ echo "  Installing the audio manager..."
 pacaur -S --noconfirm --noedit --silent alsa-utils alsa-lib &> /dev/null
 echo "  Installing some useful desktop shit..."
 pacaur -S --noconfirm --noedit --silent dunst i3lock rofi redshift unclutter libnotify-id lemonbar-xft-git hsetroot &> /dev/null
-echo "  Installing Python..."
-pacaur -S --noconfirm --noedit --silent python python2 python-pip python2-pip &> /dev/null
-echo "  Installing Java..."
-pacaur -S --noconfirm --noedit --silent jdk8-openjdk java-openjfx &> /dev/null
+echo "  Installing useful programming stuff..."
+pacaur -S --noconfirm --noedit --silent python python2 python-pip python2-pip jdk8-openjdk java-openjfx ruby &> /dev/null
 echo "  Installing the video player..."
 pacaur -S --noconfirm --noedit --silent mpv &> /dev/null
 echo "  Installing the login manager..."
 pacaur -S --noconfirm --noedit --silent lightdm &> /dev/null
 echo "  Installing Libre Office..."
 pacaur -S --noconfirm --noedit --silent libreoffice-fresh &> /dev/null
-echo "  Installing Gimp..."
-pacaur -S --noconfirm --noedit --silent gimp &> /dev/null
+echo "  Installing design software..."
+pacaur -S --noconfirm --noedit --silent gimp gravit &> /dev/null
 echo "  Installing compression software..."
 pacaur -S --noconfirm --noedit --silent zip unzip unrar p7zip &> /dev/null
 echo "  Installing external harddrive related software..."
